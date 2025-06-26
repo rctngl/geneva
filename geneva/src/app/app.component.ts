21bccb114c550d5b2a91a03e8bcdeac8f005aecf
Alex Mills <alex@acm.io>
2025-06-26 15:23:29 +0800

    Core layout and styling

--- geneva/src/app/app.component.ts
+++ geneva/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
