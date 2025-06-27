973ddf118f3f1eaecc2c68608b1100414b5837d2
Alex Mills <alex@acm.io>
2025-06-27 10:36:14 +0800

    Sidebar styling

--- geneva/src/app/app.component.ts
+++ geneva/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MobileHeaderComponent } from './components/mobile-header/mobile-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MobileHeaderComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MobileHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
